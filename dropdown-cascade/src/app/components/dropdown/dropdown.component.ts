3f1b7cb96e207c74d14e78bc4fcc3ebe3e702ba9
afdezcl <adrifdez_@uma.es>
2020-03-15 13:33:58 +0100

    Service imported to dropdown-component

--- dropdown-cascade/src/app/components/dropdown/dropdown.component.ts
+++ dropdown-cascade/src/app/components/dropdown/dropdown.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CitiesService } from 'src/app/services/cities.service';
+import {ProvinceI, CityI} from '../../models/cities.interface';
 
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.css']
+  styleUrls: ['./dropdown.component.css'],
+  providers: [CitiesService]
 })
 export class DropdownComponent implements OnInit {
 
-  constructor() { }
+  constructor(private citiesService: CitiesService) { }
 
   ngOnInit() {
+    console.log(this.citiesService.getProvinces());
+    console.log(this.citiesService.getCities());
   }
 
 }
